41755d650dc110843436bb4b5f1bf4204dfce4b0
sonicpanther101 <sonicpanther101@gmail.com>
2025-10-20 12:45:42 +1300

    starting from scratch

--- shaders/object_fragment_shader.glsl
+++
@@ -1,8 +0,0 @@
-#version 430 core
-
-in vec3 v_color;
-out vec4 fragment_color;
-
-void main() {
-    fragment_color = vec4(v_color, 1.0);
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "NN.h"
 #include <iostream>
 #include <Eigen/Dense>
 #include <vector>
@@ -8,23 +7,7 @@ const int SCREEN_HEIGHT = 1200;
 
 int main() {
 
-    NN NN(2,10,2,1, 1);
-
-    Eigen::VectorXd input(2);
-
-    input << 0, 1;
-
-    NN.SetInputValues(input);
-
-    Eigen::VectorXd output = NN.GetOutput();
-
-    std::cout << "Output Values: " << output << std::endl;
-
-    Eigen::VectorXd expectedOutput(1);
-
-    expectedOutput << 1;
-
-    NN.AdjustWeights(expectedOutput);
+    
     
     return 0;
 }
